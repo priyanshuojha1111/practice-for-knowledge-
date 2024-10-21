bd4231ad99c92661aeaba97617ed44ad1e8235a4
priyanshuojha1111 <96886444+priyanshuojha1111@users.noreply.github.com>
2024-10-21 11:43:42 +0000

    conversion of data types

---
+++ 0_1basics/0_3conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "33"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+// "33"=> 33
+//  " 33abc" => NaN
+//  true => 1
+//  false => 0
+
+let isLoggedIn = 1
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+let someone = 33 
+let StringNum = String(someone)
+console.log(StringNum);
+
+console.log(typeof StringNum);
+
+
