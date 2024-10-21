35a26ca9ca2dfdfa9dcce719d73e8623895a3e23
priyanshuojha1111 <96886444+priyanshuojha1111@users.noreply.github.com>
2024-10-21 11:08:35 +0000

    variable and datatypes

---
+++ 0_1basics/0_1datatypes.js
@@ -0,0 +1,17 @@
+let name = "Priyanshu Ojha"
+let age = 24
+let isLoggedIn = false
+let state;
+
+// number =>(range 2 to power 53)
+// bigint
+// string => ""
+// boolean =>true/false
+// null
+// undefined =>
+//symbol 
+// -----------------------------------------------------------------------------------------
+console.log(isLoggedIn)
+console.log(typeof"ojhaji")
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
---
+++ 0_1basics/0_1variables.js
@@ -0,0 +1,13 @@
+const accountId = 144132
+let accountEmail = "priyanshuojha111@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+accountEmail = "ojhaji@gmail.com"
+accountPassword = "1080"
+accountCity = "Gurgaon"
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
+
+
